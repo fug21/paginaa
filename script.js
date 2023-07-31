5864122f359272d4fb4f19595581d866e025a9b2
fug21 <141009193+fug21@users.noreply.github.com>
2023-07-31 15:34:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var disqus_config = function () {
+    this.page.url = 'https://tu-sitio-web.com/tu-pagina';  // Reemplazar con la URL canónica de tu página
+    this.page.identifier = 'tu_pagina_id'; // Reemplazar con un identificador único para esta página
+};
